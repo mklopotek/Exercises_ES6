6026536b4fc505bd03de1a3853dbc5fe06c0db07
Magda Kłopotek <magda.klopotek@gmail.com>
2018-07-19 14:05:41 +0200

    add button

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { myName as magdaName } from './mySecondModule'
 import { showMyName } from './mySecondModule'
 import myOwnName from './defaultExport'
 import allSays,{ say1, say2, say3 } from './manyExports'
+import ToDo from './ToDo'
 
 console.log(magdaName)
 
@@ -15,3 +16,4 @@ say3()
 
 console.log(allSays)
 
+new ToDo('#root')
